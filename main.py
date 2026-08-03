76e386181e8c656b30ebde5f46d6b4976fb03d18
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2026-08-03 14:58:11 +0530

    Adding Minimal Python Script For Startup

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from file-delta-to-blob!")
+# Define Main Function
+if __name__ == '__main__':
+    # Importing Python Module:S1
+    try:
+        from pathlib import Path
+    except Exception as error:
+        print(f'ERROR - [File-Delta-To-BLOB:S1] - {str(error)}')
 
-
-if __name__ == "__main__":
-    main()
+    # Define Folder Path:S2
+    try:
+        parent_folder_path = Path.cwd()
+        env_file_path = Path(parent_folder_path) / '.env'
+        file_upload_folder_path = Path(parent_folder_path) / 'FileUpload'
+        file_download_folder_path = Path(parent_folder_path) / 'FileDownload'
+        database_folder_path = Path(parent_folder_path) / 'database'
+        analysis_engine_folder_path = Path('/home/soumalya/Desktop/Office-Work/Analytics-Engine')
+    except Exception as error:
+        print(f'ERROR - [File-Delta-To-BLOB:S2] - {str(error)}')
